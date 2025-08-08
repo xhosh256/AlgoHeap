fcbf48a58d39705e1de8b1fde47b8d3089d8bc96
xhosh <lenya.kharitonov.98@gmail.com>
2025-08-09 00:15:50 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Welcome to AlgoHeap!
+
+This is my first large-scale project and i have chosen to make it a leetcode-like platform because i really love leetcode cuz i used to solve problems there every day and one day i will go back and continue solving
---
+++ src/main/java/dev/AlgoHeap/AlgoHeapApplication.java
@@ -0,0 +1,13 @@
+package dev.AlgoHeap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoHeapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoHeapApplication.class, args);
+	}
+
+}
